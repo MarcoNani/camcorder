8c7c7a6f4535dc63a08cbb0d85980d92347c719c
Marco Nani <147685388+MarcoNani@users.noreply.github.com>
2024-07-25 11:48:07 +0200

    1° sprint - Check file ash in the file and copy

--- camcorder.py
+++ camcorder.py
@@ -1,13 +1,20 @@
-import hashlib
-import os
+import hashlib # for generating file hashs
+import os # for the files paths and other useful stuffs
+import shutil # for copying files
+import filecmp # for checking if two files are identycal
 
 # CONSTANTS
 # Calculate the path of the videos on the camcorder
+IN_SECURE_MODE = True # if true the script will also check if the copy happened succesfully, it take more time
+
 ROOT_CAMCORDER = os.path.join("fs_sim","camcorder")
 REMOTE_VIDEO_DIRECTORY = os.path.join(ROOT_CAMCORDER, "PRIVATE", "AVCHD", "BDMV", "STREAM")
 
 EXTENSION = ".MTS"
 
+COPIED_FILES_LOG = os.path.join("fs_sim","pc","destination","copied_files.txt")
+
+DESTINATION_FOLDER = os.path.join("fs_sim","pc","destination")
 
 # FUNCTIONS
 
@@ -42,9 +49,47 @@ def files_in(directory, extension):
             list_of_files.append(complete_path)
     return list_of_files
 
+def copy_file(source, destination_dir, hash_file):
+    has_been_copied = False
+    # copy the file to the destination_dir folder
+    try:
+        # Copy the file while preserving metadata
+        shutil.copy2(source, destination_dir)
+        print("File copied successfully!")
+        has_been_copied = True
+        
+        if IN_SECURE_MODE:
+            # Verify if the files are identical
+            destination_file_path = os.path.join(destination_dir, os.path.basename(source))
+            if filecmp.cmp(source, destination_file_path, shallow=True): # check if the two files are identical based on metadata
+                # check if the hash passed to the function and the hash of the copied file are identical
+                if hash_file == calculate_file_hash(destination_file_path):
+                    print("Copy verification successful, the files are identical.")
+                    has_been_copied = True
+                else:
+                    print("Error: The files content (checked with the ash) are not identical after copying.")
+                    has_been_copied = False
+            else:
+                print("Error: The files metadata are not identical after copying.")
+                has_been_copied = False
 
+    except FileNotFoundError:
+        print(f"Error: The file {source} does not exist.")
+        has_been_copied = False
+    except PermissionError:
+        print("Error: Permission denied.")
+        has_been_copied = False
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        has_been_copied = False
 
-if __name__ == "__main__":
+    # add the file ash to the COPIED_FILES_LOG file if the copy happened succesfully
+    if has_been_copied:
+        with open(COPIED_FILES_LOG, 'a') as file:
+            file.write(hash_file + "\n")
+            print(f"The hash {hash_file} has been added to the copied files log")
+
+def main():
     # Iterate over all files in the directory
     video_files = files_in(REMOTE_VIDEO_DIRECTORY, EXTENSION)
 
@@ -57,3 +102,20 @@ if __name__ == "__main__":
     for video_path in video_files:
         hash_file = calculate_file_hash(video_path, 'sha256')
         print(f"The SHA-256 hash of the file {os.path.basename(video_path)} is: {hash_file}")
+        # check if the file has already been copied (looking in the COPIED_FILES_LOG)
+        if not os.path.exists(COPIED_FILES_LOG):
+            # Create the COPIED_FILES_LOG file if it doesn't exist
+            with open(COPIED_FILES_LOG, 'w') as file:
+                pass
+        with open(COPIED_FILES_LOG, 'r') as file:
+            if hash_file in file.read():
+                print(f"The file {os.path.basename(video_path)} has already been copied")
+            else:
+                print(f"The file {os.path.basename(video_path)} has not been copied yet")
+                # if the file has not been copied yet, copy it to the destination folder
+                copy_file(video_path, DESTINATION_FOLDER, hash_file)
+                
+
+
+if __name__ == "__main__":
+    main()
