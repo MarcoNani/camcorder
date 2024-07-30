f7afcbaf5c22c4b64edaa99537bf7a856b3f8c26
Marco Nani <147685388+MarcoNani@users.noreply.github.com>
2024-07-30 16:17:51 +0200

    3° sprint - GUI - done folder entry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, filedialog
 import os
 import preferences
 
@@ -7,6 +7,8 @@ import preferences
 current_preferences = {}
 
 # FUNCTIONS
+
+# -- menu functions and preferences --
 def on_exit():
     """Function to exit the application."""
     root.quit()
@@ -16,6 +18,15 @@ def on_open():
     global current_preferences
     # Load the preferences
     current_preferences = preferences.preferences_routine()
+
+    # populate the entry with the preferences
+    entry_root_camcorder.delete(0, tk.END)
+    entry_root_camcorder.insert(0, current_preferences["root_camcorder"])
+
+    entry_destination_folder.delete(0, tk.END)
+    entry_destination_folder.insert(0, current_preferences["destination_folder"])
+
+
 
 def about():
     """Show an 'About' message."""
@@ -57,9 +68,32 @@ def open_preferences_file():
     except Exception as e:
         messagebox.showerror("Error", f"Failed to open preferences file: {e}")
 
+
+# -- folder functions --
+def select_root_camcorder_folder():
+    """Function to select the root camcorder folder."""
+    folder_path = filedialog.askdirectory()
+    if folder_path:
+        entry_root_camcorder.delete(0, tk.END)
+        entry_root_camcorder.insert(0, folder_path)
+
+def select_destination_folder():
+    """Function to select the destination folder."""
+    folder_path = filedialog.askdirectory()
+    if folder_path:
+        entry_destination_folder.delete(0, tk.END)
+        entry_destination_folder.insert(0, folder_path)
+
+
+
+
 # Create the root window (the main window of the application)
 root = tk.Tk()
 root.title("Camcorder")
+
+
+
+# --- MENU BAR ---
 
 # Create the menu bar
 menu_bar = tk.Menu(root)
@@ -79,6 +113,48 @@ file_menu.add_command(label="Exit", command=on_exit)
 help_menu = tk.Menu(menu_bar, tearoff=0)
 menu_bar.add_cascade(label="Help", menu=help_menu)
 help_menu.add_command(label="About", command=about)
+
+
+
+# --- WIDGETS ---
+# columnconfigure and rowconfigure are used to configure the grid layout
+root.columnconfigure(0, minsize=100)
+root.columnconfigure(1, minsize=500)
+root.columnconfigure(2, minsize=100)
+root.rowconfigure(0, minsize=50)
+root.rowconfigure(1, minsize=50)
+root.rowconfigure(2, minsize=50)
+root.rowconfigure(3, minsize=50)
+root.rowconfigure(4, minsize=50)
+root.rowconfigure(5, minsize=50)
+root.rowconfigure(6, minsize=50)
+root.rowconfigure(7, minsize=50)
+root.rowconfigure(8, minsize=50)
+root.rowconfigure(9, minsize=50)
+
+
+# --- 1° ROW ---
+label_root_camcorder = tk.Label(root, text="Root camcorder folder:")
+label_root_camcorder.grid(row=0, column=0, sticky="e")
+
+entry_root_camcorder = tk.Entry(root)
+entry_root_camcorder.grid(row=0, column=1, sticky="ew")
+
+button_root_camcorder = tk.Button(root, text="Select folder", command=select_root_camcorder_folder)
+button_root_camcorder.grid(row=0, column=2, sticky="w")
+
+# --- 2° ROW ---
+label_destination_folder = tk.Label(root, text="Destination folder:")
+label_destination_folder.grid(row=1, column=0, sticky="e")
+
+entry_destination_folder = tk.Entry(root)
+entry_destination_folder.grid(row=1, column=1, sticky="ew")
+
+button_destination_folder = tk.Button(root, text="Select folder", command=select_destination_folder)
+button_destination_folder.grid(row=1, column=2, sticky="w")
+
+
+
 
 # Load preferences on application start
 on_open()
